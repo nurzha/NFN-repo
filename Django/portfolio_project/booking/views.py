ac082b747acf721c95c56ccda412922875ab9510
nura_alpieva <78268491+nurzha@users.noreply.github.com>
2022-01-13 11:05:23 -0600

    Update views.py
    Make changes to the views.py file on the remote depository.

--- Django/portfolio_project/booking/views.py
+++ Django/portfolio_project/booking/views.py
@@ -3,28 +3,6 @@
 # Create your views here.
 from django.http import HttpResponse
 
-#index
-#create
-#update
-#delete
-#get_by_id
-# class index(APIView):
-#     renderer_classes = [TemplateHTMLRenderer]
-#     template_name = 'tutorials/index.html'
-#     def get(self, request):
-#         queryset = Tutorial.objects.all()
-#         return Response({'tutorials': queryset})
-# class list_all_tutorials(APIView):
-#     renderer_classes = [TemplateHTMLRenderer]
-#     template_name = 'tutorials/tutorial_list.html'
-#     def get(self, request):
-#         queryset = Tutorial.objects.all()
-#         return Response({'tutorials': queryset})
 
 def index(request):
-
-    #add logic to get all booking and return it instead of the static message
-    #refer to nucamp tutrial appl file the code might hielp
-    queryset = Tutorial.objects.all()
-        # return Response({'tutorials': queryset})
-    return HttpResponse({'tutorials': queryset})
+    return HttpResponse("Hello, world. You're at the booking index.")
